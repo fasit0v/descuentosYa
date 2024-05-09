553ad317f3bc693a8cb076eb479df1d60c7b4aa5
arielAayala <ariel170802@gmail.com>
2024-05-09 20:39:10 -0300

    migrate discounts places

---
+++ database/migrations/2024_05_09_231400_create_places_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string("placeName", 100);
+            $table->decimal("placeLongitude", 5,5);
+            $table->decimal("placeLatitude", 5,5);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+};
