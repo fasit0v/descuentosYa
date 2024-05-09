e00d90266a1516bab497201a52909275e6a752b0
Ariel Alejandro Ayala <ariel170802@gmail.com>
2024-05-09 15:14:07 -0300

    google maps

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,16 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
 });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/reportes', function(){
+    return Inertia::render("Report");
+})->middleware(["auth","verified"])->name("reportes");
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
