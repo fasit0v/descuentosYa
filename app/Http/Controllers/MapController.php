ee48870ec069056cb3ff016f3edf080b7a6d1758
arielAayala <ariel170802@gmail.com>
2024-06-04 04:02:50 -0300

    map Controller

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -2,15 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Place;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class MapController extends Controller
 {
     public function index(){   
         
-        
+        $places = Place::select(
+            "places.id",
+            "places.placeName",
+            "places.placeDescription",
+            "places.placeAddress", 
+            "places.placeImage",
+            "places.placeLongitude", 
+            "places.placeLatitude", 
+            "places.placeCategory",
+            DB::raw('count(discounts.id) as discountQuantity')
+        )
+        ->leftJoin("discounts", "discounts.place_id", "=", "places.id")
+        ->groupBy("places.id", 
+            "places.placeName",
+            "places.placeDescription",
+            "places.placeAddress", 
+            "places.placeImage",
+            "places.placeLongitude", 
+            "places.placeLatitude", 
+            "places.placeCategory",)
+        ->get();
 
-        return Inertia::render("Home") ;
+
+        return Inertia::render("Home",["data"=>["places"=> $places]]) ;
     }
 }
